337ab4400c22b2e6674505af5343d70ecb2138af
Francisco Elias <franeliasbv@gmail.com>
2023-09-04 21:30:36 -0300

    Completa la primer interfaz

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -28,15 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(56, 280);
+            this.progressBar1.Location = new System.Drawing.Point(56, 286);
+            this.progressBar1.Maximum = 101;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(349, 23);
             this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // frmMain
             // 
@@ -44,13 +53,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::pryEliasIE.Properties.Resources.LogoSistemaDeSeguros;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(460, 315);
+            this.ClientSize = new System.Drawing.Size(460, 321);
             this.Controls.Add(this.progressBar1);
             this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
 
         }
@@ -58,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -16,5 +16,30 @@ namespace pryEliasIE
         {
             InitializeComponent();
         }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(5);
+
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                timer1.Stop();
+
+                frmVentanaPrincipal frmVentanaPrincipal = new frmVentanaPrincipal();
+                frmVentanaPrincipal.ShowDialog();
+
+                this.Close();
+            }
+        }
     }
 }
