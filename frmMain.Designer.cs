934b31b8fc909d44c5f1bfd13ddd574888a012bc
Francisco Elias <franeliasbv@gmail.com>
2023-09-16 16:59:08 -0300

    Cambios mínimos 3

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -51,6 +51,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = global::pryEliasIE.Properties.Resources.LogoSistemaDeSeguros;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(460, 321);
